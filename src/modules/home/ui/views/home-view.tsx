f4b1d299847f29939cf217e1a46d56217a7f2fe9
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-06-24 16:30:19 -0300

    fix: update loading and error handling in HomeView component

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -9,7 +9,7 @@ export const HomeView = () => {
   const trpc = useTRPC();
   const greeting = useQuery(trpc.hello.queryOptions({ text: "world" }));
 
-  if (!greeting.data) {
+  if (greeting.isLoading) {
     return (
       <div className="flex flex-col items-center  h-screen justify-center space-y-3">
         <Skeleton className="h-[125px] w-[250px] rounded-xl" />
@@ -20,7 +20,16 @@ export const HomeView = () => {
       </div>
     );
   }
+
+  if (greeting.isError) {
+    return (
+      <div className="flex flex-col items-center h-screen justify-center space-y-3">
+        <p className="text-red-500">Error: {greeting.error.message}</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col p-4 gap-y-4">{greeting.data.greeting}</div>
+    <div className="flex flex-col p-4 gap-y-4">{greeting.data?.greeting}</div>
   );
 };
