7a9add98bbce3927cb8db453e281c679146e86cf
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-06-25 17:31:17 -0300

    feat: add ErrorState and LoadingState components for improved UI feedback

---
+++ src/components/error-state.tsx
@@ -0,0 +1,20 @@
+import { AlertCircleIcon } from "lucide-react";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+export const ErrorState = ({ title, description }: Props) => {
+  return (
+    <div className="flex flex-1 py-4 px-8 items-center justify-center">
+      <div className="flex flex-col items-center justify-center p-10 shadow-sm bg-background rounded-b-lg gap-y-6">
+        <AlertCircleIcon className="size-6 text-destructive" />
+        <div className="flex flex-col gap-y-2 text-center">
+          <h6 className="text-lg font-medium">{title}</h6>
+          <p className="text-sm">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
